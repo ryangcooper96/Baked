aca04ee938792d45cc4b1c453b30f5b825b12f6b
Ryan Cooper <ryangcooper96@gmail.com>
2022-11-16 16:21:04 +0000

    Added CORS to project

--- Baked-Backend/project/settings.py
+++ Baked-Backend/project/settings.py
@@ -44,11 +44,13 @@ INSTALLED_APPS = [
 
     # 3rd Party Apps
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -134,3 +136,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+# CORS_ALLOWED_ORIGINS = ["http://localhost:3000", ]
+CORS_ALLOWED_ORIGINS = ['*',]
